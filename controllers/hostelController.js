6a8a535dcee9cfd98ec9fdba47d17d48bb014ac9
kamleshbhai <kamleshbhai.gorasiya@workboard.com>
2022-04-15 12:34:52 +0530

    update function change

--- controllers/hostelController.js
+++ controllers/hostelController.js
@@ -1,7 +1,7 @@
-const Hostel = require('../models/hostelModel')
-const User = require('../models/userModel');
+const Hostel = require("../models/hostelModel");
+const User = require("../models/userModel");
 
-var fs = require('fs');
+var fs = require("fs");
 
 exports.getAllHostels = async (req, res) => {
   Hostel.find()
@@ -11,7 +11,7 @@ exports.getAllHostels = async (req, res) => {
     .catch(() => {
       res.status(404).send({ message: "Unable to find" });
     });
-}
+};
 
 exports.getAllHostelsOfCollege = async (req, res) => {
   const college_id = req.params.college_id;
@@ -22,7 +22,7 @@ exports.getAllHostelsOfCollege = async (req, res) => {
     .catch(() => {
       res.status(404).send({ message: "Unable to find" });
     });
-}
+};
 
 exports.getHostelDetails = async (req, res) => {
   const college_id = req.params.college_id;
@@ -33,12 +33,12 @@ exports.getHostelDetails = async (req, res) => {
         if (val._id == hostel_id) {
           res.send(val);
         }
-      })
+      });
     })
     .catch(() => {
       res.status(404).send({ message: "Unable to find" });
     });
-}
+};
 
 exports.getFavouriteHostels = async (req, res) => {
   const user_id = req.params.user_id;
@@ -58,7 +58,7 @@ exports.getFavouriteHostels = async (req, res) => {
     .catch(() => {
       res.status(404).send({ message: "Favourite Hostel not found" });
     });
-}
+};
 
 exports.addHostel = async (req, res) => {
   const {
@@ -80,17 +80,17 @@ exports.addHostel = async (req, res) => {
   const college_id = req.params.college_id;
 
   if (req.file) {
-    var fs = require('fs');
+    var fs = require("fs");
     function base64_encode(file) {
       var bitmap = fs.readFileSync(file);
-      return new Buffer(bitmap).toString('base64');
+      return new Buffer(bitmap).toString("base64");
     }
-    var base64Str = base64_encode(req.file.path)
+    var base64Str = base64_encode(req.file.path);
     // image url
-    var url = `data:${req.file.mimetype};base64,${base64Str}`
+    var url = `data:${req.file.mimetype};base64,${base64Str}`;
   }
   const findCollege = await Hostel.findOne({ _id: college_id });
-  let hostel_image = url
+  let hostel_image = url;
   try {
     await findCollege.addHostels(
       boys,
@@ -112,10 +112,10 @@ exports.addHostel = async (req, res) => {
   } catch (error) {
     res.status(400).send({ message: error.message });
   }
-}
+};
 
 exports.updateHostel = async (req, res) => {
-
+  let id = req.params.id;
   const {
     boys,
     girls,
@@ -129,17 +129,17 @@ exports.updateHostel = async (req, res) => {
     kms,
     rooms_available,
     room_price,
-    location } = req.body
+    location,
+  } = req.body;
+
   if (req.file) {
     function base64_encode(file) {
       var bitmap = fs.readFileSync(file);
-      return new Buffer(bitmap).toString('base64');
+      return new Buffer(bitmap).toString("base64");
     }
-    var base64Str = base64_encode(req.file.path)
-    var url = `data:${req.file.mimetype};base64,${base64Str}`
+    var base64Str = base64_encode(req.file.path);
+    var url = `data:${req.file.mimetype};base64,${base64Str}`;
   }
-
-  let id = req.params.id;
 
   let newData = {
     boys: boys,
@@ -156,31 +156,83 @@ exports.updateHostel = async (req, res) => {
     room_price: room_price,
     location: location,
     _id: id,
-    hostel_image: url
-  }
+    hostel_image: url,
+  };
 
-  Hostel.update({ "hostels._id": id }, { "$set": { "hostels.$": newData } })
-    .then(college => {
-      if (college) {
-        res.send({ success: true, message: "College updated", data: college })
-      } else {
-        res.send({ success: false, message: "Invalid data" })
+  if(!req.file){
+    Hostel.update(
+      { "hostels._id": id },
+      {
+        $set: {
+          "hostels.$.boys": newData.boys,
+          "hostels.$.girls": newData.girls,
+          "hostels.$.hostel_name": newData.hostel_name,
+          "hostels.$.manager_name": newData.manager_name,
+          "hostels.$.helpline_no": newData.helpline_no,
+          "hostels.$.latitude": newData.latitude,
+          "hostels.$.longitude": newData.longitude,
+          "hostels.$.kms": newData.kms,
+          "hostels.$.rooms_available": newData.rooms_available,
+          "hostels.$.room_price": newData.room_price,
+          "hostels.$.location": newData.location,
+          "hostels.$._id":id
+        },
       }
-    })
-    .catch(e => {
-      console.log(e);
-      res.send({ success: false, err: e.message })
-    })
-}
+    )
+      .then((college) => {
+        if (college) {
+          res.send({ success: true, message: "College updated", data: college });
+        } else {
+          res.send({ success: false, message: "Invalid data" });
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send({ success: false, err: e.message });
+      });
+  }else{
+    Hostel.update(
+      { "hostels._id": id },
+      {
+        $set: {
+          "hostels.$.boys": newData.boys,
+          "hostels.$.girls": newData.girls,
+          "hostels.$.hostel_name": newData.hostel_name,
+          "hostels.$.manager_name": newData.manager_name,
+          "hostels.$.helpline_no": newData.helpline_no,
+          "hostels.$.latitude": newData.latitude,
+          "hostels.$.longitude": newData.longitude,
+          "hostels.$.kms": newData.kms,
+          "hostels.$.rooms_available": newData.rooms_available,
+          "hostels.$.room_price": newData.room_price,
+          "hostels.$.location": newData.location,
+          "hostels.$.hostel_image":newData.hostel_image,
+          "hostels.$._id":id
+        },
+      }
+    )
+      .then((college) => {
+        if (college) {
+          res.send({ success: true, message: "College updated", data: college });
+        } else {
+          res.send({ success: false, message: "Invalid data" });
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send({ success: false, err: e.message });
+      });
+  }
+};
 
 exports.removeHostel = async (req, res) => {
-  const id = req.params.id
-  Hostel.updateOne({ "hostels._id": id }, { "$pull": { "hostels": { "_id": id } } })
-    .then(data => {
-      res.send({ success: true, message: "hostel deleted" })
+  const id = req.params.id;
+  Hostel.updateOne({ "hostels._id": id }, { $pull: { hostels: { _id: id } } })
+    .then((data) => {
+      res.send({ success: true, message: "hostel deleted" });
     })
-    .catch(e => {
+    .catch((e) => {
       console.log(e);
-      res.send({ success: false, error: "error" })
-    })
-}
+      res.send({ success: false, error: "error" });
+    });
+};
--- db/db.js
+++ db/db.js
@@ -1,6 +1,14 @@
 const mongoose  = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/hostel')
+// mongoose.connect('mongodb://localhost:27017/hostel')
+// .then(()=>{
+//     console.log("Database connection successfull");
+// })
+// .catch((e)=>{
+//     console.log(e);
+// })
+
+mongoose.connect('mongodb+srv://shivam1:Shivam1@project.zpeoq.mongodb.net/hostelDB?retryWrites=true&w=majority')
 .then(()=>{
     console.log("Database connection successfull");
 })
